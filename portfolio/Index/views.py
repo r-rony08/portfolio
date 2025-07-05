1f26a33612c409673457002456882113d0959c30
r-rony08 <robiul.rony1008@gmail.com>
2025-07-05 22:54:36 +0600

    Step 4: Added template folder and rendered home.html using render()

--- portfolio/Index/views.py
+++ portfolio/Index/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Welcome to My Portfolio</h1>")
+    return render(request, 'home.html')
