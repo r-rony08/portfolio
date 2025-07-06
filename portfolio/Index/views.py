11e72f8df259439439996541e353d36b7a348855
r-rony08 <robiul.rony1008@gmail.com>
2025-07-06 21:39:57 +0600

    Step 7: Displayed projects dynamically on home page with image and download link

--- portfolio/Index/views.py
+++ portfolio/Index/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    projects = Project.objects.all()
+    return render(request, 'home.html', {'projects': projects})
+
